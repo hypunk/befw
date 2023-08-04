fce7c1e0a0fccce408f663aa5a5d483cd049f3f5
Ilya Kuznetsov <i_kuznetsov2@wargaming.net>
2023-08-04 12:24:30 +0300

    Update Logs
    Fix ipv4 regex

--- befw/state.go
+++ befw/state.go
@@ -127,7 +127,7 @@ func (state *state) modifyLocalState() {
 			logging.LogWarning(fmt.Sprintf("Can't register service %s: %s",
 				localService.Name, e.Error()))
 		} else {
-			logging.LogInfo(fmt.Sprintf("Updating local service %s", localService.Name))
+			logging.LogDebug(fmt.Sprintf("Updating local service %s", localService.Name))
 			keys[localService.Name] = localService
 		}
 	}
@@ -266,6 +266,7 @@ func refresh(configFile string) (retState *state, retError error) {
 		return state, err
 	}
 	state.saveLastState() // always
+    logging.LogDebug("Refresh done")
 	return state, nil
 }
 
